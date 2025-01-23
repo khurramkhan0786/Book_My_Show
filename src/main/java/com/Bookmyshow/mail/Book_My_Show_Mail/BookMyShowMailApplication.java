e1f36443ee99eeb6cc2abb27c26ee8b45e7eed5f
Khurram Khan <khurramkhan3838@gmail.com>
2025-01-23 15:19:26 +0530

    first commit

---
+++ src/main/java/com/Bookmyshow/mail/Book_My_Show_Mail/BookMyShowMailApplication.java
@@ -0,0 +1,13 @@
+package com.Bookmyshow.mail.Book_My_Show_Mail;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookMyShowMailApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookMyShowMailApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Bookmyshow/mail/Book_My_Show_Mail/BookMyShowMailApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Bookmyshow.mail.Book_My_Show_Mail;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookMyShowMailApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
